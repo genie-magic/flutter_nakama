396db5a30e695356c88178ecbfcd1af615666996
happy-tinker <flashingtinker@gmail.comm>
2019-03-06 02:03:34 +0800

    first commit

---
+++ ios/Classes/FlutterNakamaPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterNakamaPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/FlutterNakamaPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterNakamaPlugin.h"
+
+@implementation FlutterNakamaPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_nakama"
+            binaryMessenger:[registrar messenger]];
+  FlutterNakamaPlugin* instance = [[FlutterNakamaPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
